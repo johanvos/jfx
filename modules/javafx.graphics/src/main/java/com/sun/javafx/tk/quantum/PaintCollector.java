febe6d1ce353263ae39034a3fba92f8b4f76ca9b
Johan Vos <johan@lodgon.com>
2024-01-28 17:04:16 +0100

    Don't render on headless platform

--- modules/javafx.graphics/src/main/java/com/sun/javafx/tk/quantum/PaintCollector.java
+++ modules/javafx.graphics/src/main/java/com/sun/javafx/tk/quantum/PaintCollector.java
@@ -39,6 +39,7 @@ import com.sun.javafx.tk.RenderJob;
 
 import static com.sun.javafx.logging.PulseLogger.PULSE_LOGGING_ENABLED;
 import com.sun.javafx.logging.PulseLogger;
+import com.sun.javafx.tk.Toolkit;
 
 /**
  * Manages the collection and rendering of dirty scenes. This class has
@@ -405,9 +406,10 @@ final class PaintCollector implements CompletionListener {
                     }
                     System.err.println("[PC] i = "+i+", n = "+n+", egl? "+PlatformUtil.useEGL());
                     if (!PlatformUtil.useEGL() || i == (n - 1)) {
+if (Application.GetApplication().getClass().toString().indexOf("Headless")> -1) {
                         // for platforms without a native window manager, we only want to do the
                         // swap to the screen after the last window has been rendered
-                        if ("headless".equalsIgnoreCase(System.getProperty("glass.platform", "no"))) {
+                      //  if ("headless".equalsIgnoreCase(System.getProperty("glass.platform", "no"))) {
                             vs.setDoPresent(false);
                         } else {
                             vs.setDoPresent(true);
